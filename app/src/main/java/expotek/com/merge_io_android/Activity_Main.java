c9f2cba4e7fe37f20aa8c06a475917a1a76012e4
Craig Colquitt <ccolquitt@redzone.co>
2017-11-05 11:09:23 -0700

    Setting up wifi Connectivity

--- app/src/main/java/expotek/com/merge_io_android/Activity_Main.java
+++ app/src/main/java/expotek/com/merge_io_android/Activity_Main.java
@@ -1,19 +1,37 @@
 package expotek.com.merge_io_android;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import expotek.com.merge_io_android.io.WifiReceiver;
+
+
 public class Activity_Main extends Activity {
 
-
+    private IntentFilter mIntentFilter;
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private BroadcastReceiver mReceiver;
     private Button button_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeWifi();
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
         button_test = (Button) findViewById(R.id.activity_main_button_test);
 
@@ -30,9 +48,42 @@ public class Activity_Main extends Activity {
         }
     };
 
+    private void initializeWifi() {
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WifiReceiver(mManager, mChannel, this);
+
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+
+            }
+        });
+
+    }
+
     private boolean sendWifiMessage(){
         boolean success;
 
         return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+
+
+    }
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
     }
 }
