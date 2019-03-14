81c25c2f37e73832d5afeb41be0898a87105ed66
Craig Colquitt <colquitt.craig@gmail.com>
2019-03-13 22:02:16 -0600

    adding learn file

---
+++ merge-io-py/learn_no_normnalizaiton.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import, division, print_function
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+from numpy import array
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+class_names = ['accident' , 'no-accident']
+
+test_data = []
+with open('data_run_1.log') as my_file:
+    test_data = my_file.readlines()
+
+
+train_data = []
+train_labels = []
+for line in test_data :
+    line = line.split(',')
+    train_labels.append(line[3])
+    del line[3]
+    test_array = array(line)
+    train_data.append(test_array)
+
+train_labels = np.array(train_labels)
+train_data = np.array(train_data)
+
+model = keras.Sequential([
+    keras.layers.Dense(4, activation=tf.nn.relu),
+    keras.layers.Dense(5, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_data, train_labels, epochs=5)
+
+#test_loss, test_acc = model.evaluate(test_images, test_labels)
+
